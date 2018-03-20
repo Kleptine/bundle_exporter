af0a46a107f33f996a1d72d96f51f6939e0199fb
renderhjs <renderhjs@gmail.com>
2018-03-21 10:04:34 +1100

    - packaged file layout - fixed bug when registering classes

--- addons/FBXBundleExporter/__init__.py
+++ addons/FBXBundleExporter/__init__.py
@@ -44,8 +44,30 @@ class FBXBundleSettings(bpy.types.PropertyGroup):
 	)
 
 
+class op_export(bpy.types.Operator):
+	bl_idname = "fbxbundle.export"
+	bl_label = "Export"
+
+	def execute(self, context):
+		print ("Export Operator")
+		# export_selection(context, False)
+		return {'FINISHED'}
+
+
+
 def get_key(obj):
+	mode = bpy.context.scene.FBXBundleSettings.mode_package
+	if mode == 'name':
+		name = obj.name
+		# Remove blender naming digits, e.g. cube.001, cube.002,...
+		if len(name)>= 4 and name[-4] == '.' and name[-3].isdigit() and name[-2].isdigit() and name[-1].isdigit():
+			name = name[:-4]
+
+		split_chars = [' ','_','.','-']
+		return name
+
 	return obj.name
+
 
 
 def get_bundles():
@@ -76,7 +98,6 @@ def get_bundles():
 	keys.sort()
 	key_groups = {}
 	for key in keys:
-		
 		if key not in key_groups:
 			key_groups[key] = []
 
@@ -86,6 +107,8 @@ def get_bundles():
 				break
 
 	return key_groups
+
+
 
 class FBXBundleExporterPanel(bpy.types.Panel):
 	bl_idname = "FBX_bundle_exporter_panel"
@@ -102,15 +125,13 @@ class FBXBundleExporterPanel(bpy.types.Panel):
 		box.prop(context.scene.FBXBundleSettings, "path", text="")
 		
 		col = box.column(align=True)
-		col.label(text="Bundle mode")
-		row = col.row(align=True)
-		row.prop(context.scene.FBXBundleSettings, "mode_package", text="Mode", expand=True)
+		col.prop(context.scene.FBXBundleSettings, "mode_package", text="Bundle")
 		
 		col = box.column(align=True)
 		col.prop(context.scene.FBXBundleSettings, "mode_origin", text="Origin", expand=False)
-		layout.separator()
+		# layout.separator()
 		
-		layout.label(text="Modify")
+		layout.label(text="Add Modifier")
 		box = layout.box()
 		box.label(text="[] Copy Modifiers")
 		box.label(text="[] Merge to single Mesh")
@@ -118,17 +139,19 @@ class FBXBundleExporterPanel(bpy.types.Panel):
 		# Get bundles
 		bundles = get_bundles()
 
+		layout.operator(op_export.bl_idname, text="Export {}x".format(len(bundles)), icon='EXPORT')
+	
 
 		row = layout.row()
 		row.label('Files: '+str(len(bundles))+"x")
 		
 		if(len(bundles) > 0):
-			row = layout.row()
-			box = row.box()
-			
+
 			for fileName,objects in bundles.items():
+				row = layout.row(align=True)
+				box = row.box()
 				for i in range(0,len(objects)):
-					row = box.row()
+					row = box.row(align=True)
 					if i ==0:
 						label = text=fileName+".fbx";
 						if len(objects) > 1:
@@ -149,7 +172,7 @@ class FBXBundleExporterPanel(bpy.types.Panel):
 def register():
 	bpy.utils.register_module(__name__)
 	bpy.types.Scene.FBXBundleSettings = bpy.props.PointerProperty(type=FBXBundleSettings)
-	bpy.utils.register_class(FBXBundleExporterPanel)
+	# bpy.utils.register_class(FBXBundleExporterPanel)
 
 
 def unregister():
