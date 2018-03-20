bbfccbaedba2ccb2cbe18ae720e97ed122f196de
renderhjs <renderhjs@gmail.com>
2018-03-20 23:08:20 +1100

    panel, scene properties and registration setup for addon

--- addons/FBXBundleExporter/__init__.py
+++ addons/FBXBundleExporter/__init__.py
@@ -2,41 +2,62 @@ import bpy
 import os
 import mathutils
 
+from bpy.props import (
+	StringProperty,
+	BoolProperty,
+	IntProperty,
+	FloatProperty,
+	FloatVectorProperty,
+	EnumProperty,
+	PointerProperty,
+)
+
 bl_info = {
- "name": "FBX Bundle Exporter",
- "description": "Export object selection in FBX bundles",
- "author": "Hendrik Schoenmaker",
- "blender": (2, 7, 9),
- "version": (0, 1, 0),
- "category": "Import-Export",
- "location": "3D Viewport tools panel: FBX Bundle Exporter",
- "warning": "",
- "wiki_url": "",
- "tracker_url": "",
+	"name": "FBX Bundle Exporter",
+	"description": "Export object selection in FBX bundles",
+	"author": "Hendrik Schoenmaker",
+	"blender": (2, 7, 9),
+	"version": (0, 1, 0),
+	"category": "Import-Export",
+	"location": "3D Viewport tools panel: FBX Bundle Exporter",
+	"warning": "",
+	"wiki_url": "",
+	"tracker_url": "",
 }
 
+class FBXBundleSettings(bpy.types.PropertyGroup):
+	path = bpy.props.StringProperty (
+		name="Output Path",
+		default="",
+		description="Define the path where to export",
+		subtype='DIR_PATH'
+	)
 
-class UnityBatchExportPanel(bpy.types.Panel):
+
+class FBXBundleExporterPanel(bpy.types.Panel):
 
 	bl_idname = "FBX_bundle_exporter_panel"
-	bl_label = "FBX Bundle Exporter"
+	bl_label = "FBX Bundle"
 	bl_space_type = 'VIEW_3D'
 	bl_region_type = 'TOOLS'
 	bl_context = "objectmode"
 	# bl_options = {'DEFAULT_CLOSED'}
 
 	def draw(self, context):
-
 		layout = self.layout
 		layout.label(text="Exporting UI")
+		layout.prop(context.scene.FBXBundleSettings, "path", text="Path")
 		
 			
 # registers
 def register():
-	bpy.utils.register_class(UnityBatchExportPanel)
+	bpy.utils.register_module(__name__)
+	bpy.types.Scene.FBXBundleSettings = bpy.props.PointerProperty(type=FBXBundleSettings)
+	bpy.utils.register_class(FBXBundleExporterPanel)
 
 def unregister():
-	bpy.utils.unregister_class(UnityBatchExportPanel)
+	bpy.utils.unregister_class(FBXBundleExporterPanel)
+	del bpy.types.Scene.FBXBundleSettings
 
 if __name__ == "__main__":
 	register()
