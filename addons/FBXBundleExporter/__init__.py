407d36df66251a63f55f551d72278f3308540b1d
hendrik <hendrik.page2@gmail.com>
2018-04-03 13:40:33 +1000

    added alphabet to wireframe test to draw

--- addons/FBXBundleExporter/__init__.py
+++ addons/FBXBundleExporter/__init__.py
@@ -2,6 +2,7 @@ import bpy, bmesh
 import os
 import mathutils
 from mathutils import Vector
+import math
 
 from bpy.props import (
 	StringProperty,
@@ -182,24 +183,46 @@ class op_fence(bpy.types.Operator):
 def fence_bounds(name, bounds):
 	print("Fence {}".format(name))
 
+	padding = bpy.context.scene.FBXBundleSettings.padding
+
 	mesh = bpy.data.meshes.new("fence {}".format(name))
 	bm = bmesh.new()
 	
 	pos = bounds.center
 	min = bounds.min - pos
 	max = bounds.max - pos
+	# Expand padding
+	min-= Vector((padding,padding,0))
+	max+= Vector((padding,padding,0))
+	size = max - min
+
+	# Bottom bounds
 	add_mesh_edges(bm,
 		[min +Vector((0,0,0)),
-		min +Vector((1,0,0)),
-		min +Vector((1,1,0)),
-		min +Vector((0,1,0)),
+		min +Vector((size.x,0,0)),
+		min +Vector((size.x,size.y,0)),
+		min +Vector((0,size.y,0)),
 		min +Vector((0,0,0))]
 	)
+	add_mesh_text(bm, name)
 
 	bm.to_mesh(mesh)
 
+
 	obj = bpy.data.objects.new("fence_{}".format(name), mesh)
 	obj.location = pos
+	# obj.hide_select = True
+
+	# Add skin modifier
+	# bpy.ops.object.modifier_add(type='SKIN')
+	# bpy.context.object.modifiers["Skin"].branch_smoothing = 0
+	# bpy.ops.mesh.select_all(action='SELECT')
+	# bpy.ops.transform.skin_resize(value=(0.0568648, 0.0568648, 0.0568648), constraint_axis=(False, False, False), constraint_orientation='GLOBAL', mirror=False, proportional='DISABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
+
+
+
+
+
 	bpy.context.scene.objects.link(obj)
 
 
@@ -215,6 +238,75 @@ def add_mesh_edges(bm, points):
 		bm.edges.new((bm.verts[offset+e], bm.verts[offset+e+1]))
 
 
+
+def add_mesh_text(bm, text):
+	text = text.upper()
+	size = Vector((0.5,1))
+	offset = 0
+
+	def add_character(char, strokes):
+		nonlocal offset
+
+		print("Str {} = {}x".format(char, len(strokes)))
+		for stroke in strokes:
+			path = []
+			for id in stroke:
+				x = (id % 3)/3 * size[0] + (offset * (size[0]*1.5))
+				y = math.floor(id/3)/3 * size[1]
+				path.append(Vector((x,y,0)))
+			
+			add_mesh_edges(bm, path)
+
+
+	chars = {
+		' ':[],
+		'A':[[0,3,7,5,2],[3,5]],
+		'B':[[0,6,8,4,2,0]],
+		'C':[[2,1,3,7,8]],
+		'D':[[0,6,7,5,1,0]],
+		'E':[[2,0,6,8],[3,4]],
+		'F':[[0,6,8],[3,4]],
+		'G':[[4,5,2,0,3,7,8]],
+		'H':[[0,6],[3,5],[2,8]],
+		'I':[[0,2],[1,7],[6,8]],
+		'J':[[6,8,5,1,0,3]],
+		'K':[[6,0],[2,1,3,7,8]],
+		'L':[[6,0,2]],
+		'M':[[0,6,4,8,2]],
+		'N':[[0,6,2,8]],
+		'O':[[1,3,7,5,1]],
+		'P':[[0,6,7,5,3]],
+		'Q':[[1,3,7,5,1],[4,2]],
+		'R':[[0,6,8,4,2],[3,4]],
+		'S':[[0,1,5,3,7,8]],
+		'T':[[6,8],[7,1]],
+		'U':[[6,0,2,8]],
+		'V':[[6,3,1,5,8]],
+		'W':[[6,0,4,2,8]],
+		'X':[[6,2],[0,8]],
+		'Y':[[6,4,8],[4,1]],
+		'Z':[[6,8,0,2]],
+
+		'0':[[6,8,2,0],[0,8]],		
+		# '1':[[]],		
+
+		'?':[[3,6,8,5,4,1]]
+	}
+
+
+	for char in text:
+		# Grid Font: https://image.shutterstock.com/z/stock-vector-set-of-font-design-base-on-line-and-dot-which-represent-connection-link-and-network-vector-621619463.jpg
+		# 6 -- 7 -- 8
+		# |    |    |
+		# 3 -- 4 -- 5
+		# |    |    |
+		# 0 -- 1 -- 2
+
+		if char in chars:
+			add_character(char, chars[char])
+		else:
+			add_character('?', chars['?'])
+		offset+=1
 
 
 class op_fence_clear(bpy.types.Operator):
