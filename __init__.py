de751d77ffae3763822433de123ca0048a10a108
AquaticNightmare <troubledbass@tutanota.com>
2020-05-31 11:55:00 +0200

    - ignore updater files and increase addon version

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ bl_info = {
     "description": "Export objects in bundles",
     "author": "AquaticNightmare",
     "blender": (2, 82, 0),
-    "version": (2, 0, 0),
+    "version": (2, 1, 0),
     "category": "3D View",
     "location": "3D View > Tools Panel > Bundle Exporter",
     "warning": "",
