a8f348f953b59defd9218165a25c9fa98c7b0fc0
AquaticNightmare <troubledbass@tutanota.com>
2020-06-08 15:55:30 +0200

    triangulate modifier

---
+++ modifiers/modifier_triangulate.py
@@ -0,0 +1,33 @@
+import bpy
+import imp
+
+from . import modifier
+
+
+class BGE_mod_triangulate(modifier.BGE_mod_default):
+    label = "Triangulate"
+    id = 'triangulate'
+    url = "http://renderhjs.net/fbxbundle/"
+    type = 'MESH'
+    icon = 'MOD_TRIANGULATE'
+    tooltip = 'Applies the triangulate modifier (keeping normals)'
+
+    active: bpy.props.BoolProperty(
+        name="Active",
+        default=False
+    )
+
+    show_info: bpy.props.BoolProperty(
+        name="Show Info",
+        default=True
+    )
+
+    def _draw_info(self, layout):
+        pass
+
+    def process(self, bundle_info):
+        meshes = bundle_info['meshes']
+
+        for mesh in meshes:
+            mod = mesh.modifiers.new('export_triangulate', type='TRIANGULATE')
+            mod.keep_custom_normals = True
